92bcf0e79c1bd8934b3d4e1ac3fead708cbce5c4
unknown <wayne@.(none)>
2012-05-10 14:41:04 -0400

    Build version 1.4.8.8440.

--- Platform/TickZoomGui2/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui2/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.8.8123")]
+[assembly: AssemblyVersion("1.4.8.8440")]
 //[assembly: AssemblyFileVersion("2.0.0.0")]
