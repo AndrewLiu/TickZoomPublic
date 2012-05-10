c873a76d8c4bbb0cfd8c160502a2d3426d3c096c
unknown <wayne@.(none)>
2012-05-10 18:20:46 -0400

    Build version 1.4.8.8462.

--- Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.IBNet/Properties/AssemblyInfo.cs
+++ Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.IBNet/Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly : AssemblyVersion("1.4.8.8440")]
+[assembly : AssemblyVersion("1.4.8.8462")]
 [assembly : AssemblyFileVersion("9.6.3.14")]
 
 //CLS Compliant
--- Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
+++ Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.8.8440")]
+[assembly: AssemblyVersion("1.4.8.8462")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
