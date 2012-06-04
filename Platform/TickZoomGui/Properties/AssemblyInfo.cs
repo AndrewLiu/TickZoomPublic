ac0a484f2f847246082855d7e16498cd59ac4bd6
unknown <wayne@.(none)>
2012-06-04 07:33:37 -0400

    Build version 1.4.11.11997.

--- Platform/TickZoomGui/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using TickZoom.Api;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.11.11280")]
+[assembly: AssemblyVersion("1.4.11.11997")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
