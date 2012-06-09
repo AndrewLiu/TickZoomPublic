649af2d4efcf68248ef8ea93fb6cc2bdc758c55d
unknown <Wayne@Win7x64VS2010.(none)>
2012-06-08 20:34:23 -0400

    Build version 1.4.12.12651.

--- Platform/TickZoomGui/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using TickZoom.Api;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.12.12646")]
+[assembly: AssemblyVersion("1.4.12.12651")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Platform/TickZoomGui2/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui2/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.12.12646")]
+[assembly: AssemblyVersion("1.4.12.12651")]
 //[assembly: AssemblyFileVersion("2.0.0.0")]
