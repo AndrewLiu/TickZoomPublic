7d54c7b2ad4999936d56acc0c1f37b02383238ac
Wayne Walter <waynewalter@tickzoom.com>
2012-06-10 19:06:55 -0400

    Moved the site upload to the new file server.

--- Platform/TickZoomGui/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using TickZoom.Api;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.12.12651")]
+[assembly: AssemblyVersion("1.4.12.12937")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
+++ Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.12.12651")]
+[assembly: AssemblyVersion("1.4.12.12937")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
