82d87374c98621b665c31e9a1b3be4e66742304a
TickZoom Build Server <support@tickzoom.com>
2012-06-13 15:37:05 -0400

    Renamed assembly to MBTrading and other config for testing MBTrading.

--- Providers/MBTFIX/MBTFIXProvider/AssemblyInfo.cs
+++ Providers/MBTFIX/MBTFIXProvider/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MBTFIXProvider")]
+[assembly: AssemblyTitle("MBTProvider")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MBTFIXProvider")]
+[assembly: AssemblyProduct("MBTProvider")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
