6ed73885e07462869d20b3c7ed7d3a12c992f3a5
Wayne Walter <waynewalter@tickzoom.com>
2012-06-20 11:09:21 -0400

    Now support StringFieldEncoder.

--- Platform/TickZoomAPI1.0/Engine/EncodeHelper.cs
+++ Platform/TickZoomAPI1.0/Engine/EncodeHelper.cs
@@ -186,10 +186,43 @@ namespace TickZoom.Api
             return dymMethod;
         }
 
-        private void IncrementPtr(ILGenerator generator)
+        public static void IncrementPtr(ILGenerator generator)
+        {
+            IncrementPtr(generator,1);
+        }
+
+        public static void IncrementPtr(ILGenerator generator, int size)
         {
             generator.Emit(OpCodes.Ldloc_0);
-            generator.Emit(OpCodes.Ldc_I4_1);
+            switch( size)
+            {
+                case 1:
+                    generator.Emit(OpCodes.Ldc_I4_1);
+                    break;
+                case 2:
+                    generator.Emit(OpCodes.Ldc_I4_2);
+                    break;
+                case 3:
+                    generator.Emit(OpCodes.Ldc_I4_3);
+                    break;
+                case 4:
+                    generator.Emit(OpCodes.Ldc_I4_4);
+                    break;
+                case 5:
+                    generator.Emit(OpCodes.Ldc_I4_5);
+                    break;
+                case 6:
+                    generator.Emit(OpCodes.Ldc_I4_6);
+                    break;
+                case 7:
+                    generator.Emit(OpCodes.Ldc_I4_7);
+                    break;
+                case 8:
+                    generator.Emit(OpCodes.Ldc_I4_8);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("Unexpected increment of " + size);
+            }
             generator.Emit(OpCodes.Conv_I);
             generator.Emit(OpCodes.Add);
             generator.Emit(OpCodes.Stloc_0);
---
+++ Platform/TickZoomAPI1.0/Engine/SymbolFieldEncoder.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+
+namespace TickZoom.Api
+{
+    public class SymbolFieldEncoder : FieldEncoder
+    {
+        public void EmitLength(ILGenerator generator, FieldInfo field)
+        {
+            // SerializeString( ptr, &field)
+            generator.Emit(OpCodes.Ldloc_0);
+            generator.Emit(OpCodes.Ldloc_0);
+
+            generator.Emit(OpCodes.Ldarg_1);
+            generator.Emit(OpCodes.Ldfld, field);
+            var serializerMethod = typeof(StringFieldEncoder).GetMethod("LengthString");
+            generator.Emit(OpCodes.Call, serializerMethod);
+            generator.Emit(OpCodes.Conv_I);
+            generator.Emit(OpCodes.Add);
+            generator.Emit(OpCodes.Stloc_0);
+        }
+
+        public void EmitEncode(ILGenerator generator, FieldInfo field)
+        {
+            // SerializeString( ptr, &field)
+            generator.Emit(OpCodes.Ldloc_0);
+            generator.Emit(OpCodes.Ldloc_0);
+            generator.Emit(OpCodes.Ldarg_1);
+            generator.Emit(OpCodes.Ldfld, field);
+            var serializerMethod = typeof(StringFieldEncoder).GetMethod("SerializeString");
+            generator.Emit(OpCodes.Call, serializerMethod);
+            generator.Emit(OpCodes.Conv_I);
+            generator.Emit(OpCodes.Add);
+            generator.Emit(OpCodes.Stloc_0);
+        }
+
+        public void EmitDecode(ILGenerator generator, FieldInfo field)
+        {
+            // DeserializeString( ptr, &field)
+            generator.Emit(OpCodes.Ldloc_0);
+            generator.Emit(OpCodes.Ldloc_0);
+            generator.Emit(OpCodes.Ldarg_1);
+            generator.Emit(OpCodes.Ldflda, field);
+            var deserializerMethod = typeof(StringFieldEncoder).GetMethod("DeserializeString");
+            generator.Emit(OpCodes.Call, deserializerMethod);
+            generator.Emit(OpCodes.Conv_I);
+            generator.Emit(OpCodes.Add);
+            generator.Emit(OpCodes.Stloc_0);
+        }
+    }
+}
