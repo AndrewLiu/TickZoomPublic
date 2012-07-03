478dc9994775626d77ba2dbb437ca01971ec1cc7
TickZoom Build Server <support@tickzoom.com>
2012-07-03 13:03:04 -0400

    Build version 1.4.16.16206.

--- Platform/protobuf-net/Properties/AssemblyInfo.cs
+++ Platform/protobuf-net/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.4.16.16173")]
-[assembly: AssemblyVersion("1.4.16.16173")]
+// [assembly: AssemblyVersion("1.4.16.16206")]
+[assembly: AssemblyVersion("1.4.16.16206")]
 #if !CF
 [assembly: AssemblyFileVersion("2.0.0.480")]
 #endif
