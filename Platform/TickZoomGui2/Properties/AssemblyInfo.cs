b0b31df985e5a074171f7ca2a838cd3264b2e1f5
TickZoom Build Server <support@tickzoom.com>
2012-07-03 13:03:04 -0400

    Build version 1.4.16.16206.

--- Platform/TickZoomGui2/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui2/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.16.16173")]
+[assembly: AssemblyVersion("1.4.16.16206")]
 //[assembly: AssemblyFileVersion("2.0.0.0")]
--- Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.IBNet/Properties/AssemblyInfo.cs
+++ Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.IBNet/Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly : AssemblyVersion("1.4.16.16173")]
+[assembly : AssemblyVersion("1.4.16.16206")]
 [assembly : AssemblyFileVersion("9.6.3.14")]
 
 //CLS Compliant
