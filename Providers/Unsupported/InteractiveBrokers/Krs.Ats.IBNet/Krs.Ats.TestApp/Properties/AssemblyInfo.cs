0c82021c8ddce37a3176665f5726d49ae8653e4b
unknown <Wayne@Win7x64VS2010.(none)>
2012-07-20 06:02:03 -0400

    Build version 1.4.18.18612.

--- Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
+++ Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.18.18599")]
+[assembly: AssemblyVersion("1.4.18.18612")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
