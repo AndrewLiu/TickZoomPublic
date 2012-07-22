fa10404a4489ab776c49d5a53c7847fe51aaf231
Wayne Walter <waynewalter@tickzoom.com>
2012-07-22 19:48:21 -0400

    Build version 1.4.18.18982.

--- Platform/TickZoomGui/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using TickZoom.Api;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.18.18979")]
+[assembly: AssemblyVersion("1.4.18.18982")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
