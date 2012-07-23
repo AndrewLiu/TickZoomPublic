7a65a79f3f5a6d7b2792c1971190a3307c659101
unknown <Wayne@Win7x64VS2010.(none)>
2012-07-23 09:53:04 -0400

    Build version 1.4.18.19067.

--- Platform/Warehouse/AssemblyInfo.cs
+++ Platform/Warehouse/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.4.18.18982")]
+[assembly: AssemblyVersion("1.4.18.19067")]
--- Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
+++ Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.18.18982")]
+[assembly: AssemblyVersion("1.4.18.19067")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
