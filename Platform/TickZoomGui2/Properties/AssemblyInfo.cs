9273ff68e73890b89f8ac9f49a114faeae81897a
unknown <Wayne@Win7x64VS2010.(none)>
2012-07-23 12:26:36 -0400

    Build version 1.4.18.19082.

--- Platform/TickZoomGui2/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui2/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.18.19080")]
+[assembly: AssemblyVersion("1.4.18.19082")]
 //[assembly: AssemblyFileVersion("2.0.0.0")]
--- Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
+++ Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.TestApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.18.19080")]
+[assembly: AssemblyVersion("1.4.18.19082")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
