e4afebf993bd32953fde6137bf4492e010d55051
unknown <Wayne@Win7x64VS2010.(none)>
2012-09-19 10:26:16 -0400

    Build version 1.4.27.27422.

--- Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.IBNet/Properties/AssemblyInfo.cs
+++ Providers/Unsupported/InteractiveBrokers/Krs.Ats.IBNet/Krs.Ats.IBNet/Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly : AssemblyVersion("1.4.26.27086")]
+[assembly : AssemblyVersion("1.4.27.27422")]
 [assembly : AssemblyFileVersion("9.6.3.14")]
 
 //CLS Compliant
